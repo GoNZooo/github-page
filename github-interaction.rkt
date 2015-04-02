425984851ba02479fdba8ce67c6bd511e2a4fcdb
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-04-02 17:24:04 +0300

    Put some finishing touches on interface names

--- github-interaction.rkt
+++ github-interaction.rkt
@@ -7,23 +7,32 @@
 		 json
 		 net/url)
 
+(provide (struct-out user)
+		 (struct-out repo)
+		 api/user->user
+		 api/repos->repos)
+
 (define github-base-url "https://api.github.com/")
 (define github-user-url (string-append github-base-url
 									   "user"))
 (define github-repos-url (string-append github-user-url
 										"/repos"))
 
-(define (auth-token-loc)
+(define/contract (auth-token-loc)
+  (-> string?)
+  
   (bytes->string/utf-8
 	(environment-variables-ref (current-environment-variables)
 							   #"GITHUB_REPO_TOKEN_LOC")))
 
-(define (auth-token-value)
+(define/contract (auth-token-value)
+  (-> string?)
+
   (string-replace (call-with-input-file (auth-token-loc)
 										port->string)
 				  "\n" ""))
 
-(define/contract (fetch/user [token (auth-token-value)])
+(define/contract (api/user [token (auth-token-value)])
   (() (string?) . ->* . (or/c jsexpr? eof-object?))
   
   (call/input-url (string->url github-user-url)
@@ -32,7 +41,7 @@
 				  `(,(format "Authorization: token ~a"
 							 (auth-token-value)))))
 
-(define/contract (fetch/repos [token (auth-token-value)])
+(define/contract (api/repos [token (auth-token-value)])
   (() (string?) . ->* . (or/c jsexpr? eof-object?))
   
   (call/input-url (string->url github-repos-url)
@@ -44,7 +53,9 @@
 (struct user (name location html-url)
 		#:transparent)
 
-(define (json/user->user json-data)
+(define/contract (api/user->user [json-data (api/user)])
+  (() (jsexpr?) . ->* . user?)
+
   (define name (hash-ref json-data 'name))
   (define location (hash-ref json-data 'location))
   (define html-url (hash-ref json-data 'html_url))
@@ -53,16 +64,22 @@
 (struct repo (name description html-url language)
 		#:transparent)
 
-(define (json/repo->repo json-data)
+(define/contract (api/repo->repo json-data)
+  (() (jsexpr?) . ->* . repo?)
+  
   (define name (hash-ref json-data 'name))
   (define description (hash-ref json-data 'description))
   (define html-url (hash-ref json-data 'html_url))
   (define language (hash-ref json-data 'language))
   (repo name description html-url language))
 
-(define (json/repos->repos json-data)
-  (map json/repo->repo json-data))
+(define/contract (api/repos->repos [json-data (api/repos)])
+  (() ((listof jsexpr?)) . ->* . (listof repo?))
+  
+  (map api/repo->repo json-data))
 
 (module+ main
-  (json/user->user (fetch/user))
-  (json/repos->repos (fetch/repos)))
+  (require racket/pretty)
+  
+  (api/user->user)
+  (pretty-print (api/repos->repos)))
