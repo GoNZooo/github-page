dedd4a6664c9e2ef573662f60d8a461f7caba973
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-04-05 10:58:46 +0300

    Created file for token interaction

---
+++ github-api/token.rkt
@@ -0,0 +1,21 @@
+#lang racket/base
+
+(require racket/port
+		 racket/string)
+
+(provide auth-token-value)
+
+(define/contract (auth-token-loc)
+  (-> string?)
+  
+  (bytes->string/utf-8
+	(environment-variables-ref (current-environment-variables)
+							   #"GITHUB_REPO_TOKEN_LOC")))
+
+(define/contract (auth-token-value)
+  (-> string?)
+
+  (string-replace (call-with-input-file (auth-token-loc)
+										port->string)
+				  "\n" ""))
+
