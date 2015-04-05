10d3241c05750ad1b1df16c741dba2b5f396072a
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-04-05 11:08:14 +0300

    Moved fetching function out to separate module to be required in others

---
+++ github-api/fetcher.rkt
@@ -0,0 +1,41 @@
+#lang racket/base
+
+(require net/url
+		 json)
+
+(define/contract (api/fetch type [cache? #t])
+  ((string?) (boolean?) . ->* . jsexpr?)
+
+  (define github-base-url "https://api.github.com/")
+
+  (define (get-public-events-url)
+	(format "~ausers/~a/events/public"
+			github-base-url
+			(hash-ref (api/user) 'login)))
+
+  (define (api-url)
+	(case type
+	  [("user") (string-append github-base-url
+							   "user")]
+	  [("repos") (string-append github-base-url
+								"user/repos")]
+	  [("email") (string-append github-base-url
+								"user/emails")]
+	  [("events") (get-public-events-url)]))
+
+  (define-values (api-port header-string)
+	(get-pure-port/headers (string->url (api-url))
+						   (list (format "Authorization: token ~a"
+										 (auth-token-value))
+								 (format "If-None-Match: ~a"
+										 (read-etag type)))))
+
+  (define header (header-string->header-list header-string))
+
+  (if (and (not-modified? header) cache?)
+	(read-cache type)
+	(let ([js-data (read-json api-port)])
+	  (close-input-port api-port)
+	  (write-cache type js-data)
+	  (write-etag type (extract-etag header))
+	  js-data)))
