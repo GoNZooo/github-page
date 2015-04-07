9e73eab7239e8ae280862e96251b2f789eb621ca
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-04-07 23:47:03 +0300

    Added compose-events-url to handle url creation

--- github-api/urls.rkt
+++ github-api/urls.rkt
@@ -4,11 +4,13 @@
 		 net/url)
 
 (provide compose-user-url
-		 compose-repos-url)
+		 compose-repos-url
+		 compose-events-url)
 
 (define urls
   '#hash((user . "https://api.github.com/users/~a")
-		 (repos . "https://api.github.com/users/~a/repos")))
+		 (repos . "https://api.github.com/users/~a/repos")
+		 (events . "https://api.github.com/users/~a/events")))
 
 (define/contract (compose-user-url login)
   (string? . -> . url?)
@@ -21,5 +23,12 @@
   
   (string->url (format (hash-ref urls 'repos)
 					   login)))
+
+(define/contract (compose-events-url login)
+  (string? . -> . url?)
+  
+  (string->url (format (hash-ref urls 'events)
+					   login)))
+
 (module+ main
   (url->string (compose-user-url "GoNZooo")))
