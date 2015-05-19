6bcee63b7c32b47425613eceba3e51d32fc47b3e
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-05-19 12:28:31 +0300

    Fixed formatting in token.rkt

--- github-api/token.rkt
+++ github-api/token.rkt
@@ -1,22 +1,22 @@
 #lang racket/base
 
 (require racket/port
-		 racket/string
-		 racket/contract)
+         racket/string
+         racket/contract)
 
 (provide auth-token-value)
 
 (define/contract (auth-token-loc)
   (-> string?)
-  
+
   (bytes->string/utf-8
-	(environment-variables-ref (current-environment-variables)
-							   #"GITHUB_REPO_TOKEN_LOC")))
+    (environment-variables-ref (current-environment-variables)
+                               #"GITHUB_REPO_TOKEN_LOC")))
 
 (define/contract (auth-token-value)
   (-> string?)
 
   (string-replace (call-with-input-file (auth-token-loc)
-										port->string)
-				  "\n" ""))
+                                        port->string)
+                  "\n" ""))
 
