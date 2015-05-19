54cbd923567ca08c40db31ba4244b1bab24ebd7b
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-05-19 12:29:38 +0300

    Fixed formatting in cache.rkt

--- github-api/cache.rkt
+++ github-api/cache.rkt
@@ -1,27 +1,27 @@
 #lang racket/base
 
 (require racket/port
-		 racket/contract
-		 racket/string
-		 net/url
-		 json)
+         racket/contract
+         racket/string
+         net/url
+         json)
 
 (provide read-cache
-		 write-cache
-		 cache-exists?
-		 cache-name)
+         write-cache
+         cache-exists?
+         cache-name)
 
 (define/contract (cache-name url)
   (url? . -> . string?)
 
   (define (strip-url u)
-	(string-replace (string-replace (url->string u)
-									#px".*://"
-									"")
-					#rx"/" "_"))
-  
+    (string-replace (string-replace (url->string u)
+                                    #px".*://"
+                                    "")
+                    #rx"/" "_"))
+
   (format "cache/~a.cache"
-		  (strip-url url)))
+          (strip-url url)))
 
 (define/contract (cache-exists? url)
   (url? . -> . boolean?)
@@ -30,14 +30,14 @@
 
 (define/contract (read-cache url)
   (url? . -> . jsexpr?)
-  
+
   (call-with-input-file (cache-name url)
-						read))
+                        read))
 
 (define/contract (write-cache url data)
   (url? jsexpr? . -> . void?)
-  
+
   (call-with-output-file (cache-name url)
-						 (lambda (out-port)
-						   (write data out-port))
-						 #:exists 'replace))
+                         (lambda (out-port)
+                           (write data out-port))
+                         #:exists 'replace))
