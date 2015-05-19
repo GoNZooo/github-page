3723f166ddab1716cdbc2eaede1f4ff934e18474
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-05-19 12:29:06 +0300

    Fixed formatting in events.rkt

--- github-api/events.rkt
+++ github-api/events.rkt
@@ -1,85 +1,85 @@
 #lang racket/base
 
 (require racket/contract
-		 racket/match
-		 json
+         racket/match
+         json
 
-		 "urls.rkt"
-		 "fetcher.rkt")
+         "urls.rkt"
+         "fetcher.rkt")
 
 (provide github/events
-		 (struct-out actor)
-		 (struct-out event/repo)
-		 (struct-out event))
+         (struct-out actor)
+         (struct-out event/repo)
+         (struct-out event))
 
 (struct actor (avatar-url
-				gravatar-id
-				id
-				login
-				url)
-		#:transparent)
+                gravatar-id
+                id
+                login
+                url)
+        #:transparent)
 
 (struct event/repo (id
-					name
-					url)
-		#:transparent)
+                     name
+                     url)
+        #:transparent)
 
 (struct event (created-at
-				id
-				payload
-				public
-				repo
-				type)
-		#:transparent)
+                id
+                payload
+                public
+                repo
+                type)
+        #:transparent)
 
 
 (define/contract (github/events login
-								#:token [token ""])
+                                #:token [token ""])
   ((string?) (#:token string?) . ->* . (listof event?))
 
   (define/contract (js-event->event js-event)
-	(jsexpr? . -> . event?)
+    (jsexpr? . -> . event?)
 
-	(match js-event
-	  [(hash-table
-		 ('actor
-		  (hash-table
-			('avatar_url avatar-url)
-			('gravatar_id gravatar-id)
-			('id actor-id)
-			('login login)
-			('url actor-url)))
-		 ('created_at created-at)
-		 ('id id)
-		 ('payload payload)
-		 ('public public)
-		 ('repo
-		  (hash-table
-			('id repo-id)
-			('name name)
-			('url repo-url)))
-		 ('type type))
-	   (event
-		 (actor avatar-url
-				gravatar-id
-				actor-id
-				login
-				actor-url)
-		 id
-		 payload
-		 public
-		 (event/repo
-		   repo-id
-		   name
-		   repo-url)
-		 type)]))
+    (match js-event
+      [(hash-table
+         ('actor
+          (hash-table
+            ('avatar_url avatar-url)
+            ('gravatar_id gravatar-id)
+            ('id actor-id)
+            ('login login)
+            ('url actor-url)))
+         ('created_at created-at)
+         ('id id)
+         ('payload payload)
+         ('public public)
+         ('repo
+          (hash-table
+            ('id repo-id)
+            ('name name)
+            ('url repo-url)))
+         ('type type))
+       (event
+         (actor avatar-url
+                gravatar-id
+                actor-id
+                login
+                actor-url)
+         id
+         payload
+         public
+         (event/repo
+           repo-id
+           name
+           repo-url)
+         type)]))
 
   (map js-event->event
-	   (github/fetch (compose-events-url login)
-					 #:token token))) 
+       (github/fetch (compose-events-url login)
+                     #:token token))) 
 
 (module+ main
   (require racket/pretty
-		   "token.rkt")
-  
+           "token.rkt")
+
   (pretty-print (github/events "GoNZooo")))
