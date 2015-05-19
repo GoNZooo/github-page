ca31bebf1c4d098a47c3748f0ba856ff70dc3eae
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-05-19 12:28:17 +0300

    Fixed formatting in urls.rkt

--- github-api/urls.rkt
+++ github-api/urls.rkt
@@ -1,34 +1,34 @@
 #lang racket/base
 
 (require racket/contract
-		 net/url)
+         net/url)
 
 (provide compose-user-url
-		 compose-repos-url
-		 compose-events-url)
+         compose-repos-url
+         compose-events-url)
 
 (define urls
   '#hash((user . "https://api.github.com/users/~a")
-		 (repos . "https://api.github.com/users/~a/repos")
-		 (events . "https://api.github.com/users/~a/events")))
+         (repos . "https://api.github.com/users/~a/repos")
+         (events . "https://api.github.com/users/~a/events")))
 
 (define/contract (compose-user-url login)
   (string? . -> . url?)
-  
+
   (string->url (format (hash-ref urls 'user)
-					   login)))
+                       login)))
 
 (define/contract (compose-repos-url login)
   (string? . -> . url?)
-  
+
   (string->url (format (hash-ref urls 'repos)
-					   login)))
+                       login)))
 
 (define/contract (compose-events-url login)
   (string? . -> . url?)
-  
+
   (string->url (format (hash-ref urls 'events)
-					   login)))
+                       login)))
 
 (module+ main
   (url->string (compose-user-url "GoNZooo")))
