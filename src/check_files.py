2ad34a1f28dd88fbd2d070b21d11b90771a11b44
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-07-03 09:58:41 +0300

    Fix width of check-files.py to not go over 80

--- src/check_files.py
+++ src/check_files.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     # Check for necessary token environment variable, used for
     # fetching data from GitHub specific to the user.
     if not os.getenv("GITHUB_REPO_TOKEN_LOC", False):
-        print "Cannot find value for environment variable 'GITHUB_REPO_TOKEN_LOC'. This should be set."
+        print "Cannot find value for environment variable" + \
+                "'GITHUB_REPO_TOKEN_LOC'. This should be set."
     elif not os.access(os.getenv("GITHUB_REPO_TOKEN_LOC", False), os.R_OK):
         print "File that 'GITHUB_REPO_TOKEN_LOC' points to is not readable."
 
