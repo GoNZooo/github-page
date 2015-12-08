c46af73b5c34b1fe24aa8d615eafdd5634e9c36c
Rickard Andersson <gonz@severnatazvezda.com>
2015-12-08 02:04:47 +0200

    Reindent events.rkt

--- src/github-api/events.rkt
+++ src/github-api/events.rkt
@@ -13,24 +13,24 @@
          (struct-out event))
 
 (struct actor (avatar-url
-                gravatar-id
-                id
-                login
-                url)
-        #:transparent)
+               gravatar-id
+               id
+               login
+               url)
+  #:transparent)
 
 (struct event/repo (id
-                     name
-                     url)
-        #:transparent)
+                    name
+                    url)
+  #:transparent)
 
 (struct event (created-at
-                id
-                payload
-                public
-                repo
-                type)
-        #:transparent)
+               id
+               payload
+               public
+               repo
+               type)
+  #:transparent)
 
 
 (define/contract (github/events login
@@ -42,37 +42,37 @@
 
     (match js-event
       [(hash-table
-         ('actor
-          (hash-table
-            ('avatar_url avatar-url)
-            ('gravatar_id gravatar-id)
-            ('id actor-id)
-            ('login login)
-            ('url actor-url)))
-         ('created_at created-at)
-         ('id id)
-         ('payload payload)
-         ('public public)
-         ('repo
-          (hash-table
-            ('id repo-id)
-            ('name name)
-            ('url repo-url)))
-         ('type type))
+        ('actor
+         (hash-table
+          ('avatar_url avatar-url)
+          ('gravatar_id gravatar-id)
+          ('id actor-id)
+          ('login login)
+          ('url actor-url)))
+        ('created_at created-at)
+        ('id id)
+        ('payload payload)
+        ('public public)
+        ('repo
+         (hash-table
+          ('id repo-id)
+          ('name name)
+          ('url repo-url)))
+        ('type type))
        (event
-         (actor avatar-url
-                gravatar-id
-                actor-id
-                login
-                actor-url)
-         id
-         payload
-         public
-         (event/repo
-           repo-id
-           name
-           repo-url)
-         type)]))
+        (actor avatar-url
+               gravatar-id
+               actor-id
+               login
+               actor-url)
+        id
+        payload
+        public
+        (event/repo
+         repo-id
+         name
+         repo-url)
+        type)]))
 
   (map js-event->event
        (github/fetch (compose-events-url login)
