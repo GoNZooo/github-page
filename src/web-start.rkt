c6ed8cd5958bf79c97c7902360cf9d5d3efcf082
Rickard Andersson <gonz@severnatazvezda.com>
2015-12-08 02:01:36 +0200

    Reindent web-start.rkt

--- src/web-start.rkt
+++ src/web-start.rkt
@@ -10,10 +10,10 @@
 
 (define/page (main-page user-data repo-list)
   (response/full
-    200 #"Okay"
-    (current-seconds) TEXT/HTML-MIME-TYPE
-    '()
-    `(,(string->bytes/utf-8 (include-template "templates/main.html")))))
+   200 #"Okay"
+   (current-seconds) TEXT/HTML-MIME-TYPE
+   '()
+   `(,(string->bytes/utf-8 (include-template "templates/main.html")))))
 
 (define (request/github request)
   (main-page request
@@ -22,18 +22,18 @@
 
 (define/page (ping-page)
   (response/full
-    200 #"Okay"
-    (current-seconds) TEXT/HTML-MIME-TYPE
-    '()
-    `(,(string->bytes/utf-8 "Pong!"))))
+   200 #"Okay"
+   (current-seconds) TEXT/HTML-MIME-TYPE
+   '()
+   `(,(string->bytes/utf-8 "Pong!"))))
 
 (define (request/ping request)
   (ping-page request))
 
 (define-values (github-page-dispatch github-page-url)
   (dispatch-rules
-    [("ping") request/ping]
-    [("github" "") request/github]))
+   [("ping") request/ping]
+   [("github" "") request/github]))
 
 (serve/servlet github-page-dispatch
                #:port 8080
